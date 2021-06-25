9b41fe42599566a57af9c11d3eb031587374ccb1
mahmoud <mahmoud.samy@black-squares.com>
2021-06-25 21:57:33 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,10 @@ export default defineComponent({
             titleTemplate: title => `${title} - Resume`,
             meta: {
                 keywords    : { name         : 'keywords'     , content: 'arch fedora centos docker jenkins php python ruby go javascript dart kotlin laravel gin django rails adonis mysql sqlite neo4j firebase mongodb postgressql git composer npm yarn pip pacman dnf yum gem ffmpeg' },
-                equiv       : { 'http-equiv' : 'Content-Type' , content: 'text/html; charset=UTF-8'   },
-                ogImage     : { property     : 'og:image'     , content: require( `assets/icon.png` ) },
-                description : { name         : 'description'  , content: 'Mahmoud Sami Resume'        },
-                ogTitle     : { property     : 'og:title'     , content: `Mahmoud Sami Resume`        }
+                equiv       : { 'http-equiv' : 'Content-Type' , content: 'text/html; charset=UTF-8'        },
+                description : { name         : 'description'  , content: 'Mahmoud Sami Resume'             },
+                ogTitle     : { property     : 'og:title'     , content: `Mahmoud Sami Resume`             },
+                ogImage     : { property     : 'og:image'     , content: require( `src/statics/icon.png` ) },
             },
         })
     ],
